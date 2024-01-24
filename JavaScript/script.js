680887a59c73f887bb66a95367ca29e6a43990b5
Arsik3204 <xhasik@bk.ru>
2024-01-24 13:33:50 +0300

    burger menu and bug fix

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function openWindow () {
+    let modal = document.getElementById("burger");
+    modal.style.display = "block";
+}
+
+function closeWindow() {
+    let modal = document.getElementById("burger");
+    modal.style.display = "none";
+}
