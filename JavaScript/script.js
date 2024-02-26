737117993883943c52791a49ab4fbba58ab7ac5a
Arsik3204 <xhasik@bk.ru>
2024-02-27 00:03:00 +0300

    tabsJS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,42 @@
 "use strict";
 
-function openWindow () {
+const openWindow = () => {
     let modal = document.getElementById("burger");
     modal.style.display = "block";
 }
 
-function closeWindow() {
+const closeWindow = () => {
     let modal = document.getElementById("burger");
     modal.style.display = "none";
 }
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+ const handleTabClick = (event) => {
+    // Прячем все элменты tab panels (содержимое табов)
+    tabPanels.forEach(panel => {
+        panel.hidden = true;
+    });
+    // Устанавливаем состояние selected для всех табов равное false
+    tabButtons.forEach(tab => {
+        // tab.ariaSelected = false;
+        tab.setAttribute('aria-selected', false);
+    });
+    // Для "кликнутого" таба устанвливаем значение selected равное true
+    event.currentTarget.setAttribute('aria-selected', true);
+
+    // Находим tabPanel для "кликнутого" таба и отображаем
+    const { id } = event.currentTarget;
+
+    console.log(tabPanels);
+
+    const tabPanel = tabPanels.find(
+        panel => panel.getAttribute('aria-labelledby') === id
+    );
+    tabPanel.hidden = false;
+}
+
+tabButtons
+    .forEach(button => button.addEventListener('click', handleTabClick));
